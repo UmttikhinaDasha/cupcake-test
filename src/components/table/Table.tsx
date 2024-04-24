0600aefb5b53335187d52c9b278ffacef9c94c82
dasha <darya.125viis.467@gmail.com>
2024-04-24 22:06:52 +0700

    adding data rendering

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,6 +1,29 @@
+import { useState } from 'react'
+import { usePollingRequest } from '../../hooks/usePollingRequest'
+
 import './table.scss'
+import clsx from 'clsx'
 
 export const Table = () => {
+    const firstRates = usePollingRequest('first', 'first/poll')
+    const secondRates = usePollingRequest('second', 'second/poll')
+    const thirdRates = usePollingRequest('third', 'third/poll')
+
+    const renderTableValues = (items: number[]) => {
+        const minValue = Math.min.apply(null, items)
+
+        return items.map((item, index) => (
+            <td
+                key={index}
+                className={clsx(
+                    'table__body-item',
+                    minValue == item && 'table__body-item_active'
+                )}>
+                {item}
+            </td>
+        ))
+    }
+
     return (
         <table className='table'>
             <thead className='table__head'>
@@ -15,21 +38,57 @@ export const Table = () => {
             <tbody className='table__body'>
                 <tr>
                     <td className='table__body-item'>RUB/CUPCAKE</td>
+                    {renderTableValues([
+                        Number(firstRates.RUB.toFixed(3)),
+                        Number(secondRates?.RUB?.toFixed(3)),
+                        Number(thirdRates?.RUB?.toFixed(3)),
+                    ])}
                 </tr>
                 <tr>
                     <td className='table__body-item'>USD/CUPCAKE</td>
+                    {renderTableValues([
+                        Number(firstRates?.USD?.toFixed(3)),
+                        Number(secondRates?.USD?.toFixed(3)),
+                        Number(thirdRates?.USD?.toFixed(3)),
+                    ])}
                 </tr>
                 <tr>
                     <td className='table__body-item'>EUR/CUPCAKE</td>
+                    {renderTableValues([
+                        Number(firstRates?.EUR?.toFixed(3)),
+                        Number(secondRates?.EUR?.toFixed(3)),
+                        Number(thirdRates?.EUR?.toFixed(3)),
+                    ])}
                 </tr>
                 <tr>
                     <td className='table__body-item'>RUB/USD</td>
+                    {renderTableValues([
+                        Number((firstRates?.RUB / firstRates?.USD).toFixed(3)),
+                        Number(
+                            (secondRates?.RUB / secondRates?.USD).toFixed(3)
+                        ),
+                        Number((thirdRates?.RUB / thirdRates?.USD).toFixed(3)),
+                    ])}
                 </tr>
                 <tr>
                     <td className='table__body-item'>RUB/EUR</td>
+                    {renderTableValues([
+                        Number((firstRates?.RUB / firstRates?.EUR).toFixed(3)),
+                        Number(
+                            (secondRates?.RUB / secondRates?.EUR).toFixed(3)
+                        ),
+                        Number((thirdRates?.RUB / thirdRates?.EUR).toFixed(3)),
+                    ])}
                 </tr>
                 <tr>
                     <td className='table__body-item'>EUR/RUB</td>
+                    {renderTableValues([
+                        Number((firstRates?.EUR / firstRates?.RUB).toFixed(3)),
+                        Number(
+                            (secondRates?.EUR / secondRates?.RUB).toFixed(3)
+                        ),
+                        Number((thirdRates?.EUR / thirdRates?.RUB).toFixed(3)),
+                    ])}
                 </tr>
             </tbody>
         </table>
